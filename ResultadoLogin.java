a704e06b13e8285f9ffe5eacef555d9a96eecc0f
barojas04 <barojas04@gmail.com>
2024-11-21 17:13:17 -0600

    Create ResultadoLogin.java

---
+++ ResultadoLogin.java
@@ -0,0 +1,17 @@
+public class ResultadoLogin {
+    private final boolean exitoso;
+    private final String mensaje;
+
+    public ResultadoLogin(boolean exitoso, String mensaje) {
+        this.exitoso = exitoso;
+        this.mensaje = mensaje;
+    }
+
+    public boolean esExitoso() {
+        return exitoso;
+    }
+
+    public String obtenerMensaje() {
+        return mensaje;
+    }
+}
